bd32fa19b74ec251437ba8717e15b7252e8cae6e
user5095 <140261471+user5095@users.noreply.github.com>
2024-02-20 16:03:20 +0100

    Delete process-login.php

--- process-login.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Verifica se la password è stata inviata e se è corretta
-if (isset($_POST['password']) && $_POST['password'] === 'montanellipetrarca') {
-    // Password corretta, reindirizza l'utente alla pagina desiderata
-    header('Location: https://user5095.github.io/Interrogazioni/sito.html');
-    exit;
-}
-?>
