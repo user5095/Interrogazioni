cde3e447e7c120dac1429cd4f68ef85c6d073aa1
user5095 <140261471+user5095@users.noreply.github.com>
2024-02-20 16:00:09 +0100

    Update process-login.php

--- process-login.php
+++ process-login.php
@@ -4,3 +4,5 @@ if (isset($_POST['password']) && $_POST['password'] === 'montanellipetrarca') {
     // Password corretta, reindirizza l'utente alla pagina desiderata
     header('Location: https://user5095.github.io/Interrogazioni/sito.html');
     exit;
+}
+?>
